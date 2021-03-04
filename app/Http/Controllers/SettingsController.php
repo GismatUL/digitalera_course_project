db9a589cf53d763341a5bb3773f29f3e54c9e9a2
EyCoder <qmammadov539@gmail.com>
2021-03-04 14:44:16 +0400

    update error

--- app/Http/Controllers/SettingsController.php
+++ app/Http/Controllers/SettingsController.php
@@ -14,22 +14,59 @@ class SettingsController extends Controller
 
     public function store(Request $request)
     {
+        $check_table = Settings::count();
         $settings = new Settings();
-        $settings->menu_1 = $request->menu_1;
-        $settings->menu_2 = $request->menu_2;
-        $settings->menu_3 = $request->menu_3;
-        $settings->location_title = $request->location_title;
-        $settings->location_content = $request->location_content;
-        $settings->social_icons_title = $request->social_icons_title;
-        $settings->fb_url = $request->fb_url;
-        $settings->tw_url = $request->fb_url;
-        $settings->in_url = $request->in_url;
-        $settings->db_url = $request->db_url;
-        $settings->footer_about_title = $request->footer_about_title;
-        $settings->footer_about_content = $request->footer_about_content;
-        $settings->footer_content = $request->footer_about_content;
-        $settings->save();
 
+        if ($request->hasFile('image')) {
+            $request->validate(['image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',]);
+            $imageName = time() . '.' . $request->image->extension();
+
+            $request->image->storeAs('public/portfolio/images',$imageName);
+            $settings->logo = asset('storage/portfolio/images/'.$imageName);
+            $request->image = asset('storage/portfolio/images/'.$imageName);
+        }
+
+        {
+            $request->image = $request->current_image;
+        }
+        if ($check_table)
+        {
+            $get_id = Settings::first();
+
+            Settings::where(['id'=>$get_id->id])->update([
+                'menu_1' => $request->menu_1,
+                'menu_2' => $request->menu_2,
+                'menu_3' => $request->menu_3,
+                'location_title' => $request->location_title,
+                'location_content' => $request->location_content,
+                'social_icons_title' => $request->social_icons_title,
+                'fb_url' => $request->fb_url,
+                'tw_url' => $request->fb_url,
+                'in_url' => $request->in_url,
+                'db_url' => $request->db_url,
+                'footer_about_title' => $request->footer_about_title,
+                'footer_about_content' => $request->footer_about_content,
+                'footer_content' => $request->footer_about_content,
+                'logo' => $request->image,
+            ]);
+        }
+            else
+            {
+                $settings->menu_1 = $request->menu_1;
+                $settings->menu_2 = $request->menu_2;
+                $settings->menu_3 = $request->menu_3;
+                $settings->location_title = $request->location_title;
+                $settings->location_content = $request->location_content;
+                $settings->social_icons_title = $request->social_icons_title;
+                $settings->fb_url = $request->fb_url;
+                $settings->tw_url = $request->fb_url;
+                $settings->in_url = $request->in_url;
+                $settings->db_url = $request->db_url;
+                $settings->footer_about_title = $request->footer_about_title;
+                $settings->footer_about_content = $request->footer_about_content;
+                $settings->footer_content = $request->footer_about_content;
+                $settings->save();
+            }
         return redirect()->back()->with('flash_message_success','Data has successfully been inserted');
     }
 }
--- resources/views/admin/settings.blade.php
+++ resources/views/admin/settings.blade.php
@@ -1,3 +1,10 @@
+@php
+use App\Settings;
+    $counter = Settings::count();
+    if ($counter)
+        $settings = Settings::first();
+@endphp
+
 @extends('layouts.admin_layout.admin_design')
 @section('content')
     <div class="breadcrumbs">
@@ -32,101 +39,110 @@
                             <strong>Masked Input</strong> <small></small>
                         </div>
                         <div class="card-body card-block">
-                            <form action="" method="POST">
+                            <form action="" method="POST" enctype="multipart/form-data">
                                 {{csrf_field()}}
                             <div class="form-group">
                                 <label class=" form-control-label">Menu_1</label>
                                 <div class="input-group">
                                     <div class="input-group-addon"><i class="fa fa-menu"></i></div>
-                                    <input type="text" class="form-control" name="menu_1">
+                                    <input type="text" class="form-control" name="menu_1" @if($counter) value="{{$settings->menu_1}}" @endif>
                                 </div>
                             </div>
                             <div class="form-group">
                                 <label class=" form-control-label">Menu_2</label>
                                 <div class="input-group">
                                     <div class="input-group-addon"><i class="fa fa-calendar"></i></div>
-                                    <input type="text" class="form-control" name="menu_2">
+                                    <input type="text" class="form-control" name="menu_2" @if($counter) value="{{$settings->menu_2}}" @endif>
                                 </div>
                             </div>
                             <div class="form-group">
                                 <label class=" form-control-label">Menu_3</label>
                                 <div class="input-group">
                                     <div class="input-group-addon"><i class="fa fa-calendar"></i></div>
-                                    <input type="text" class="form-control" name="menu_3">
+                                    <input type="text" class="form-control" name="menu_3" @if($counter) value="{{$settings->menu_3}}" @endif>
                                 </div>
                             </div>
                             <div class="form-group">
                                 <label class=" form-control-label">Location_title</label>
                                 <div class="input-group">
                                     <div class="input-group-addon"><i class="fa fa-phone"></i></div>
-                                    <input type="text" class="form-control" name="location_title">
+                                    <input type="text" class="form-control" name="location_title" @if($counter) value="{{$settings->location_title}}" @endif>
                                 </div>
                             </div>
                             <div class="form-group">
                                 <label class=" form-control-label">Location_content</label>
                                 <div class="input-group">
                                     <div class="input-group-addon"><i class="fa fa-phone"></i></div>
-                                    <input type="text" class="form-control" name="location_content">
+                                    <input type="text" class="form-control" name="location_content" @if($counter) value="{{$settings->location_content}}" @endif>
                                 </div>
                             </div>
                             <div class="form-group">
                                 <label class=" form-control-label">Social Icons Title</label>
                                 <div class="input-group">
                                     <div class="input-group-addon"><i class="fa fa-phone"></i></div>
-                                    <input type="text" class="form-control" name="social_icons_title">
+                                    <input type="text" class="form-control" name="social_icons_title" @if($counter) value="{{$settings->social_icons_title}}" @endif>
                                 </div>
                             </div>
                             <div class="form-group">
                                 <label class=" form-control-label">Facebook</label>
                                 <div class="input-group">
                                     <div class="input-group-addon"><i class="fa fa-phone"></i></div>
-                                    <input type="text" class="form-control" name="fb_url">
+                                    <input type="text" class="form-control" name="fb_url" @if($counter) value="{{$settings->fb_url}}" @endif >
                                 </div>
                             </div>
                             <div class="form-group">
                                 <label class=" form-control-label">Twitter</label>
                                 <div class="input-group">
                                     <div class="input-group-addon"><i class="fa fa-phone"></i></div>
-                                    <input type="text" class="form-control" name="tw_url">
+                                    <input type="text" class="form-control" name="tw_url" @if($counter) value="{{$settings->fb_url}}" @endif >
                                 </div>
                             </div>
                             <div class="form-group">
                                 <label class=" form-control-label">Linkendin</label>
                                 <div class="input-group">
                                     <div class="input-group-addon"><i class="fa fa-phone"></i></div>
-                                    <input type="text" class="form-control" name="in_url">
+                                    <input type="text" class="form-control" name="in_url" @if($counter) value="{{$settings->fb_url}}" @endif >
                                 </div>
                             </div>
                             <div class="form-group">
                                 <label class=" form-control-label">Dribbble</label>
                                 <div class="input-group">
                                     <div class="input-group-addon"><i class="fa fa-phone"></i></div>
-                                    <input type="text" class="form-control" name="db_url">
+                                    <input type="text" class="form-control" name="db_url" @if($counter) value="{{$settings->fb_url}}" @endif >
                                 </div>
                             </div>
                             <div class="form-group">
                                 <label class=" form-control-label">Footer About title</label>
                                 <div class="input-group">
                                     <div class="input-group-addon"><i class="fa fa-phone"></i></div>
-                                    <input type="text" class="form-control" name="footer_about_title">
+                                    <input type="text" class="form-control" name="footer_about_title" @if($counter) value="{{$settings->fb_url}}" @endif >
                                 </div>
                             </div>
                             <div class="form-group">
-                                <label class=" form-control-label">Dribbble</label>
+                                <label class=" form-control-label">Footer About Content</label>
                                 <div class="input-group">
                                     <div class="input-group-addon"><i class="fa fa-phone"></i></div>
-                                    <input type="text" class="form-control" name="footer_about_content">
+                                    <input type="text" class="form-control" name="footer_about_content" @if($counter) value="{{$settings->fb_url}}" @endif >
                                 </div>
                             </div>
+                                <div class="form-group">
+                                    <label class=" form-control-label">Footer Content</label>
+                                    <div class="input-group">
+                                        <div class="input-group-addon"><i class="fa fa-phone"></i></div>
+                                        <input type="text" class="form-control" name="footer_content" @if($counter) value="{{$settings->fb_url}}" @endif >
+                                    </div>
+                                </div>
                             <div class="form-group">
                                 <label class=" form-control-label">Logo</label>
                                 <div class="input-group">
                                     <div class="input-group-addon"><i class="fa fa-phone"></i></div>
-                                    <input type="file" class="form-control" name="logo">
+                                    <input type="file" class="form-control" name="image">
+                                    <input type="hidden" class="form-control" name="current_image" @if($counter) value="{{$settings->logo}}" @endif>
+                                    <img class="align-self-center rounded-circle mr-3" style="width:85px; height:85px;" alt="" @if($counter) src="{{$settings->logo}}" @endif >
                                 </div>
                             </div>
                                 <div class="card-body">
-                                    <button type="submit" class="btn btn-primary">Primary</button>
+                                    <button type="submit" class="btn btn-primary">UPDATE</button>
                                 </div>
                             </form>
                         </div>
